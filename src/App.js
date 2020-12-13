c3e34279047c3b1d244903e229b3f7c0cbbecd60
Aneudy Adames <a4aneudy@gmail.com>
2020-12-13 04:42:49 -0500

    added porperty lazy to the img tag

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React,{useState,useEffect,Suspense} from 'react'
+import React, { useState, useEffect, Suspense } from 'react';
 import './App.css';
 
-const PokemonComponent = React.lazy(()=> import('./pokemonCard')) 
+const PokemonComponent = React.lazy(() => import('./pokemonCard'));
 
 function App() {
-  const [pkms, setpkms]= useState([])
+  const [pkms, setpkms] = useState([]);
 
-  useEffect(  () => {
-    const fetchPkm =  async () => {
-       const results =  await  fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
-       const resultsJson = await results.json()
-        setpkms(resultsJson.results)
-      }
-      fetchPkm()
-        }, [])
+  useEffect(() => {
+    const fetchPkm = async () => {
+      const results = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151'
+      );
+      const resultsJson = await results.json();
+      setpkms(resultsJson.results);
+    };
+    fetchPkm();
+  }, []);
 
- 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Pokedex</h1>
       <div className='pokemons'>
         <Suspense fallback={<div>...Loading</div>}>
-      {pkms.map(pkm=>{
-   return <PokemonComponent key={pkm.name} {...pkm}/>
-      })}
-      </Suspense>
+          {pkms.map((pkm) => {
+            return <PokemonComponent key={pkm.name} {...pkm} />;
+          })}
+        </Suspense>
       </div>
     </div>
   );
--- src/pokemonCard.js
+++ src/pokemonCard.js
@@ -1,31 +1,32 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
-const PokemonCard = ({name,url}) => {
-    const [pkmData , setPkmData] = useState([])
-    useEffect(() => {
-        const fetchPkmData = async () => {
-            const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-            const resultsJson = await results.json()
-            setPkmData(resultsJson)
+const PokemonCard = ({ name, url }) => {
+  const [pkmData, setPkmData] = useState([]);
+  useEffect(() => {
+    const fetchPkmData = async () => {
+      const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const resultsJson = await results.json();
+      setPkmData(resultsJson);
+    };
+    fetchPkmData();
+  }, [name]);
 
-        }
-        fetchPkmData()
-        }
-    ,[name])
-
-   
-    const pkmImg = pkmData.id ? `https://pokeres.bastionbot.org/images/pokemon/${pkmData.id}.png` : null
-    const pkmType = pkmData.types  ? pkmData.types[0].type.name : 'normal'
-    return <div className={`pokemon-card ${pkmType}`}>
-        <div className='img-container'>
-            <img width='150' src={pkmImg} alt={name}/>
-        </div>
-        <div className='pokemon-info'>
-            <h2 className='name'>{name}</h2>
-            <p className='number'>#{pkmData.id}</p>
-            <p className='type'>Type: {pkmType}</p>
-        </div>
+  const pkmImg = pkmData.id
+    ? `https://pokeres.bastionbot.org/images/pokemon/${pkmData.id}.png`
+    : null;
+  const pkmType = pkmData.types ? pkmData.types[0].type.name : 'normal';
+  return (
+    <div className={`pokemon-card ${pkmType}`}>
+      <div className='img-container'>
+        <img width='150' loading='lazy' src={pkmImg} alt={name} />
+      </div>
+      <div className='pokemon-info'>
+        <h2 className='name'>{name}</h2>
+        <p className='number'>#{pkmData.id}</p>
+        <p className='type'>Type: {pkmType}</p>
+      </div>
     </div>
-}
+  );
+};
 
-export default React.memo(PokemonCard)
+export default React.memo(PokemonCard);
