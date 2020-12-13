baef0e3d54820e144e9396e1a7da0cf1d8c53fb7
Aneudy Adames <a4aneudy@gmail.com>
2020-12-13 04:09:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React,{useState,useEffect,Suspense} from 'react'
 import './App.css';
 
+const PokemonComponent = React.lazy(()=> import('./pokemonCard')) 
+
 function App() {
+  const [pkms, setpkms]= useState([])
+  // const [allTypes, setAllTypes]= useState([])
+
+  useEffect(  () => {
+    const fetchPkm =  async () => {
+       const results =  await  fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
+       const resultsJson = await results.json()
+        setpkms(resultsJson.results)
+      }
+      fetchPkm()
+        }, [])
+
+  // useEffect(() => {
+  //   fetch('https://pokeapi.co/api/v2/type/').then(data=>{
+  //     return data.json()
+  //   }).then(types=>setAllTypes(types.results))
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokedex</h1>
+      <div className='pokemons'>
+        <Suspense fallback={<div>...Loading</div>}>
+      {pkms.map(pkm=>{
+   return <PokemonComponent key={pkm.name} {...pkm}/>
+      })}
+      </Suspense>
+      </div>
     </div>
   );
 }
---
+++ src/pokemonCard.js
@@ -0,0 +1,31 @@
+import React,{useEffect,useState} from 'react'
+
+const PokemonCard = ({name,url}) => {
+    const [pkmData , setPkmData] = useState([])
+    useEffect(() => {
+        const fetchPkmData = async () => {
+            const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            const resultsJson = await results.json()
+            setPkmData(resultsJson)
+
+        }
+        fetchPkmData()
+        }
+    ,[name])
+
+   
+    const pkmImg = pkmData.id ? `https://pokeres.bastionbot.org/images/pokemon/${pkmData.id}.png` : null
+    const pkmType = pkmData.types  ? pkmData.types[0].type.name : 'normal'
+    return <div className={`pokemon-card ${pkmType}`}>
+        <div className='img-container'>
+            <img src={pkmImg} alt={name}/>
+        </div>
+        <div className='pokemon-info'>
+            <h2 className='name'>{name}</h2>
+            <p className='number'>#{pkmData.id}</p>
+            <p className='type'>Type: {pkmType}</p>
+        </div>
+    </div>
+}
+
+export default React.memo(PokemonCard)
